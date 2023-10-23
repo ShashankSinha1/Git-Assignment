caca7ccae08f1e83935d77e219319d2605254584
Shashank Sinha <shashank.wmr@gmail.com>
2023-10-23 14:37:05 -0400

    My second unit test

---
+++ simpleProgTest2.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class simpleProgTest2 {
+    @Test
+    public void testMultiply() {
+        assertEquals(90, simpleProg.add(50, 40));
+    }
+}
