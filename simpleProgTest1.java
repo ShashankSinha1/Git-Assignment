853bdb106e9c4349c012f7366f0ada8a4b7f59cc
Shashank Sinha <shashank.wmr@gmail.com>
2023-10-23 14:36:42 -0400

    My first unit test

---
+++ simpleProgTest1.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class simpleProgTest1 {
+    @Test
+    public void testMultiply() {
+        assertEquals(12, simpleProg.add(5, 7));
+    }
+}
